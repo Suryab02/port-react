1e0f407793c5d231b2165a9e134feff0717f306f
Suryab02 <bsprabhas2002@gmail.com>
2024-02-15 22:43:08 +0530

    added typewriter

--- src/components/FirstFrame.jsx
+++ src/components/FirstFrame.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import styles from "./first.module.css";
+import Typewriter from "typewriter-effect"; // Import Typewriter from the correct package
 
 function FirstFrame() {
   return (
     <div>
       <section id={styles.herosection}>
         <video id={styles.videobackground} autoPlay loop muted>
-          <source
-            src="/video/sky.mp4"
-            type="video/mp4"
-          />
+          <source src="/video/sky.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <h1 id={styles.heroname}>Surya Prabhas Bandaru</h1>
+        <div className={styles.navbar}>
+          <div className={styles.name}>Surya Prabhas Bandaru</div>
+          <ul className={styles.navbarLinks}>
+            <li className={styles.active}>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={styles.centered}>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("HEY I'M")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("SURYA PRABHAS BANDARU")
+                .pauseFor(1000)
+                .start();
+            }}
+          />
+        </div>
       </section>
     </div>
   );
